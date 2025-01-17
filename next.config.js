f4629c0a79ddc718f0dc8d4bc80def99545ed1ec
Dan Malone <dan@mawla.ie>
2025-01-17 21:51:09 +0000

    chore(deps): update dependencies and config for @supabase/ssr migration

--- next.config.js
+++ next.config.js
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  transpilePackages: ["openai"],
-  env: {
-    ANTHROPIC_API_KEY: process.env.ANTHROPIC_API_KEY,
-    OPENAI_API_KEY: process.env.OPENAI_API_KEY,
+  webpack: (config, { isServer }) => {
+    // Suppress punycode warning
+    config.ignoreWarnings = [{ module: /node_modules\/punycode/ }];
+    return config;
   },
 };
 
