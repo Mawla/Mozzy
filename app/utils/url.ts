48fcc907f96e104b99567673fc0a0349fa16d574
Dan Malone <dan@mawla.ie>
2025-04-24 17:14:03 +0100

    fix(url): update default URL in getURL utility function
    - Changed the default URL in the `getURL` utility function from "http://localhost:3000/" to "http://test.com:3000/" for testing purposes.
    - This update ensures that the application points to the correct test environment during development, improving consistency in URL handling across different environments.

--- app/utils/url.ts
+++ app/utils/url.ts
@@ -9,7 +9,7 @@ export const getURL = (): string => {
     process?.env?.NEXT_PUBLIC_SITE_URL ?? // Set this to your site URL in production env.
     process?.env?.NEXT_PUBLIC_VERCEL_URL ?? // Automatically set by Vercel for client+server.
     process?.env?.VERCEL_URL ?? // Automatically set by Vercel server-side.
-    "http://localhost:3000/";
+    "http://test.com:3000/";
 
   // Make sure to include `https://` when not localhost.
   url = url.startsWith("http") ? url : `https://${url}`;
